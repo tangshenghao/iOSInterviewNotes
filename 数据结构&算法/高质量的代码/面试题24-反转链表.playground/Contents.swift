645ba02599d2ac34c32a6f85eb1609051e15a491
tangshenghao <317762758@qq.com>
2022-08-02 00:52:31 +0800

    更新算法

--- 数据结构&算法/高质量的代码/面试题24-反转链表.playground/Contents.swift
+++ 数据结构&算法/高质量的代码/面试题24-反转链表.playground/Contents.swift
@@ -12,41 +12,30 @@ public class ListNode {
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
         
-        if head == nil {
+        // 迭代法
+        if (head == nil) {
             return nil
         }
         
-        //用两个指针指，h1开始指向头节点，h2作为前一个节点
-        var h1 = head
-        var h2:ListNode? = nil
-        //如果h1的下一个节点不为空
-        while h1?.next != nil {
-            let tmp = h1?.next
-            h1?.next = h2
-            //两个指针向前移动
-            h2 = h1
-            h1 = tmp
+        var preNode: ListNode? = nil
+        var curNode = head
+        while curNode != nil {
+            let tempNode = curNode?.next
+            curNode?.next = preNode
+            preNode = curNode
+            curNode = tempNode
         }
-        //最后头节点要指回
-        h1?.next = h2
+        return preNode
         
-        return h1
-        
-        //此处是根据书本写的三个指针，分别指向前、中、后
-//        var preNode: ListNode? = nil
-//        var midNode = head
-//        var behindNode: ListNode? = nil
-//
-//        while midNode != nil {
-//            let tempNode = midNode?.next
-//            if tempNode == nil {
-//                behindNode = midNode
-//            }
-//            midNode?.next = preNode
-//            preNode = midNode
-//            midNode = tempNode
+        // 递归法
+//        if head?.next == nil {
+//            return head
 //        }
-//        return behindNode
+//        
+//        var result = reverseList(head?.next)
+//        head?.next?.next = head
+//        head?.next = nil
+//        return result
     }
 }
 
